17ffc93ba3a5e3746627390c83c9f5d80686f6c8
fkoca <ferdikoca12@gmail.com>
2023-12-24 20:31:34 +0300

    cookie and logout

--- TravelerBlog/Controllers/SecurityController.cs
+++ TravelerBlog/Controllers/SecurityController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using TravelerBlog.Entity;
 using TravelerBlog.Models;
 
@@ -25,6 +26,7 @@ namespace TravelerBlog.Controllers
             {
                 return RedirectToAction("Register", "Security");
             }
+            FormsAuthentication.SetAuthCookie(Email, true);
             return RedirectToAction("Index", "Home");
         }
 
@@ -46,8 +48,13 @@ namespace TravelerBlog.Controllers
             };
 
             _userProcess.Add(user);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Security");
         }
 
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
